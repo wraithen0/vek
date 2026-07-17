86eae22d9bcf6504aab89cd42096af2f9c234832
Alish-0x <hyp@pulseonix.xyz>
2026-07-17 10:20:05 +0545

    CI: Enable AVX-512 build to match test_backends.c expectations

---
+++ build_cmake/CMakeFiles/bench_kernels.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bench_kernels.dir/link.d"
+  "CMakeFiles/bench_kernels.dir/bench/bench_kernels.c.o"
+  "CMakeFiles/bench_kernels.dir/bench/bench_kernels.c.o.d"
+  "bin/bench_kernels"
+  "bin/bench_kernels.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bench_kernels.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build_cmake/CMakeFiles/vek_shared.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vek_shared.dir/link.d"
+  "CMakeFiles/vek_shared.dir/src/dispatch.c.o"
+  "CMakeFiles/vek_shared.dir/src/dispatch.c.o.d"
+  "CMakeFiles/vek_shared.dir/src/scalar/kernels.c.o"
+  "CMakeFiles/vek_shared.dir/src/scalar/kernels.c.o.d"
+  "CMakeFiles/vek_shared.dir/src/x86/avx2.c.o"
+  "CMakeFiles/vek_shared.dir/src/x86/avx2.c.o.d"
+  "CMakeFiles/vek_shared.dir/src/x86/sse2.c.o"
+  "CMakeFiles/vek_shared.dir/src/x86/sse2.c.o.d"
+  "lib/libvek_shared.pdb"
+  "lib/libvek_shared.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vek_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
